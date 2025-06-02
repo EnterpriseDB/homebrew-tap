7c491b6777f689bac1cad9927652844affa7a0cc
Phil Eaton <phil.eaton@enterprisedb.com>
2025-06-02 13:36:29 -0400

    Fix url

--- Formula/pgd-cli.rb
+++ Formula/pgd-cli.rb
@@ -10,7 +10,7 @@ class PgdCli < Formula
     depends_on "libpq"
 
     if Hardware::CPU.arm?
-      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/files/pgd-cli_5.8.0_darwin_arm64.tar.gz", using: HomebrewCurlDownloadStrategy
+      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/5.8.0-1/pgd-cli_5.8.0_darwin_arm64.tar.gz", using: HomebrewCurlDownloadStrategy
       sha256 "dd1a6872aaaa446f4e3cd383c50e238946e4a9d0387db5fe3da63f3757f4d4b4"
 
       def install
@@ -21,7 +21,7 @@ class PgdCli < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/files/pgd-cli_5.8.0_darwin_amd64.tar.gz", using: HomebrewCurlDownloadStrategy
+      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/5.8.0-1/pgd-cli_5.8.0_darwin_amd64.tar.gz", using: HomebrewCurlDownloadStrategy
       sha256 "1f066e55c42230ff6f7d439752fb2617b945f135398ffdce7ba4ebf0ba256ada"
 
       def install
