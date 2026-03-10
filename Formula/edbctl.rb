b99b0920b132c05fcb88fa4e3cc69bbc664452f6
PC-2NR0VQ3\wai.wong <wai.wong@enterprisedb.com>
2026-03-10 10:51:36 +0000

    1.2.1 edbctl release homebrew curl fix

--- Formula/edbctl.rb
+++ Formula/edbctl.rb
@@ -10,7 +10,7 @@ class Edbctl < Formula
     depends_on "libpq"
 
     if Hardware::CPU.arm?
-      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/1.2.0-1/edbctl_1.2.0_darwin_arm64.tar.gz", using: HomebrewCurlDownloadStrategy
+      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/1.2.0-1/edbctl_1.2.0_darwin_arm64.tar.gz", using: CurlDownloadStrategy
       sha256 "9144b145438a7c2d0c3843594cf80b6d8ca05590c43491944541565fcdbdeb5b"
 
       def install
@@ -19,7 +19,7 @@ class Edbctl < Formula
     end
 
     if Hardware::CPU.intel?
-      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/1.2.0-1/edbctl_1.2.0_darwin_amd64.tar.gz", using: HomebrewCurlDownloadStrategy
+      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/1.2.0-1/edbctl_1.2.0_darwin_amd64.tar.gz", using: CurlDownloadStrategy
       sha256 "b80477051cec49600a5be0c436a531667b48ede58f66aa722ee5b755059ef621"
       
       def install
