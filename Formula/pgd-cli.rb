2834fb170948a0d7c12355feb47972697adfdd6d
Jaroslava Schovancová <jaroslava.schovancova@enterprisedb.com>
2026-03-11 09:21:29 +0100

    Updating CurlDownloadStrategy

--- Formula/pgd-cli.rb
+++ Formula/pgd-cli.rb
@@ -10,7 +10,7 @@ class PgdCli < Formula
     depends_on "libpq"
 
     if Hardware::CPU.arm?
-      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/6.2.0-1/pgd-cli_6.2.0_darwin_arm64.tar.gz", using: HomebrewCurlDownloadStrategy
+      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/6.2.0-1/pgd-cli_6.2.0_darwin_arm64.tar.gz", using: CurlDownloadStrategy
       sha256 "aad93b088cff5c7c15350a97f9162989e76e2ba0a3de28b7ac387f591f8b48ce"
 
       def install
@@ -21,7 +21,7 @@ class PgdCli < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/6.2.0-1/pgd-cli_6.2.0_darwin_amd64.tar.gz", using: HomebrewCurlDownloadStrategy
+      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/6.2.0-1/pgd-cli_6.2.0_darwin_amd64.tar.gz", using: CurlDownloadStrategy
       sha256 "f448a4bd8d6edeba3d6c95384cb86cee6d9d6ff138d87bd3d4b6e1c40b62009b"
 
       def install
