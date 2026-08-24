b382f9ac9e5eef799ebe84a3d34ee60e3ee788a9
DJavorek00 <dominik.javorek@enterprisedb.com>
2026-08-24 13:17:55 +0200

    Release 6.5.0 related changes

--- Formula/pgd-cli.rb
+++ Formula/pgd-cli.rb
@@ -4,14 +4,14 @@
 class PgdCli < Formula
   desc "Postgres Distributed CLI"
   homepage "https://github.com/enterprisedb/homebrew-tap"
-  version "6.4.0"
+  version "6.5.0"
 
   on_macos do
     depends_on "libpq"
 
     if Hardware::CPU.arm?
-      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/6.4.0-1/pgd-cli_6.4.0_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "ff3712f5b521a5259e2ac9f548b9d83d2a532baba29193d1d2d73034325ab5ea"
+      url "https://downloads.enterprisedb.com/public/homebrew-edb/raw/versions/6.5.0-1/pgd-cli_6.5.0_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "fc39011740ee71c3405b6f4b82ab3f9936c69864f2e598dce0e0a78924d426ea"
 
       def install
         bin.install "pgd"
